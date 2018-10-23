670592dbce080333457c544c791d68956d59e1e9
Junichi Kawahara <kawahara-j@hi-ad.jp>
2018-10-23 10:01:22 +0900

    first commit

---
+++ pages/users/_id.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="container">
+    <div>
+        <h3>{{ user.id }}</h3>
+        <img src:="user.profile_image_url" width="120" alt="">
+        <p>{{ user.description || 'No description' }}</p>
+        <p>
+            <nuxt-link to="/">
+                <small><b> トップへ戻る</b></small>
+            </nuxt-link>
+        </p>
+    </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userId: this.$route.params.id
+        }
+    }
+}
+</script>
